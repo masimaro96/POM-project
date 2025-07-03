21729df1a380146693b248ba56cb8dd810b96f8a
masimaro96 <61616531+masimaro96@users.noreply.github.com>
2025-07-03 13:12:35 +0700

    Delete auto_POM directory

--- auto_POM/locators/login_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LoginLocator:
-    URL = "https://vndev.hanbiro.com/ngw/app"
-    USERNAME_INPUT = (By.ID, "log-userid")
-    FRAME_PASSWORD = (By.ID, "iframeLoginPassword")
-    PASSWORD_INPUT = (By.ID, "p")
-    LOGIN_BUTTON = (By.ID, "btn-log")
